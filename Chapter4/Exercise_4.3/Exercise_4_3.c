f70bb0f8cee830a82a17299bc5e6922d964bf882
imakshatt <forstudygate@gmail.com>
2024-07-22 10:53:29 +0530

    Update

--- Chapter4/Exercise_4.3/Exercise_4_3.c
+++ Chapter4/Exercise_4.3/Exercise_4_3.c
@@ -38,26 +38,29 @@ int main(){
 
 	while((type=getop(s)) != EOF){
 		switch(type){
-			case NUMBER:
-				push(atof(s));
+			case NUMBER: /*if type == '0' then this case performs*/
+				push(atof(s)); /*converting s into double and push to stack*/
+				break; /*break the case*/
+			case '+':   /*If type == '+' then we have to perform Addition*/
+				push(pop() + pop()); /*Pop first 2 ele and perform '+'*/
+				break; /*Break the case*/
+			case '*': /*If type == '*' then we have to perform Multiplication*/
+				push(pop() * pop()); /*pop first 2 ele and perform '*'*/
 				break;
-			case '+':
-				push(pop() + pop());
-				break;
-			case '*':
-				push(pop() * pop());
-				break;
-			case '-':
-				op2 = pop();
-				push(pop() - op2);
-				break;
-			case '/':
-				op2 = pop();
-				if(op2 != 0.0){
-					push(pop() / op2);
+			case '-': /*If type == '-' then perform Substraction*/
+				/*As in Substraction Sign and order metters hense use op2*/
+				op2 = pop(); /*store top ele of stack in op2*/
+				push(pop() - op2); /*Now pop last ele and perform '-'*/
+				break; 
+			case '/': /*If type == '/' then perform Division*/
+				/*As in Division Sign and order merrers*/
+				/*And also encounter zero division error*/
+				op2 = pop(); /*Store first element of stack in op2*/
+				if(op2 != 0.0){ /*Chekc if op2==0 then '/' Not possible*/
+					push(pop() / op2); /*Perform '/'*/
 				}
 				else{
-					printf("Error: Zero Division Error\n");
+					printf("Error: Zero Division Error\n"); /*Error Handling*/
 				}
 			case '%':
 				op2 = pop();
@@ -91,30 +94,33 @@ int getop(char s[]){
 
 	while((s[0] = c = getch()) == ' ' || c == '\t');  /*For Skipping Space and Tab*/
 
-	s[1] = '\0';
+	s[1] = '\0'; /*Make s[1] as EOF for temp*/
 
-	if(!isdigit(c) && c !='.' && c != '-'){
-		return c;
+	if(!isdigit(c) && c !='.' && c != '-'){ /*Case for Handling Non Digit Char
+	                                          If !isdigit(c) then retrun that operator*/
+		return c;  /*Retrun to main and stored in type*/
 	}
 
-	i=0;
-
-	if(c == '-'){
-		char next = getch();
-		if(isdigit(next) || next == '.'){
-			s[++i] = c = next;
+	i=0; /*Initializing i as ZERO*/
+ 
+	/*For Handle Negative Character*/
+	/*Because there are 2 possibilities One is -Ve Operand and -ve is also Operator*/
+	if(c == '-'){  /*Check as C is '-' or not*/
+		char next = getch();  /*If it is then store the next value in next variable*/
+		if(isdigit(next) || next == '.'){ /*Now check for that NEXT value like isdigit & .*/
+			s[++i] = c = next; /*then store that in s[]*/
 		}
 		else{
-			ungetch(next);
-			return c;
+			ungetch(next); /*Otherwise handling the space using ungetch()*/
+			return c; /*And if that is operator then return That Sign*/
 		}
 	}
 
-	if(isdigit(c)){
-		while(isdigit(s[++i] = c = getch()));
+	if(isdigit(c)){ /*This is the case for handling Digit*/
+		while(isdigit(s[++i] = c = getch())); /*continue until we get digit*/
 	}
 
-	if(c == '.'){
+	if(c == '.'){ /*For fractional part*/
 		while(isdigit(s[++i] = c = getch()));
 	}
 
@@ -125,12 +131,12 @@ int getop(char s[]){
 		return '-';
 	}*/
 
-	s[i] = '\0';
+	s[i] = '\0'; /*At the End at ith location in s[] mark as EOF*/
 
-	if(c != EOF){
-		ungetch(c);
+	if(c != EOF){  /*If C is not an EOF then*/
+		ungetch(c); /*Handle it in Buffer*/
 	}
-	return NUMBER;
+	return NUMBER; /*At the End Retrun the number stored in s[] as '0' for switch case*/
 }
 
 
